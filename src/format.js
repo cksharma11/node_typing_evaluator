1788face260899b35af49959419d86d8c966f5a7
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-28 13:48:57 +0530

    Format code

--- src/format.js
+++ src/format.js
@@ -2,13 +2,13 @@ const chalk = require('chalk');
 const Table = require('cli-table3');
 
 const table = new Table({
-    head:['Accuracy',"Speed (WPM)"],
-    colWidths:[40,40]
-})
+  head: ['Accuracy', 'Speed (WPM)'],
+  colWidths: [40, 40]
+});
 
 const format = function(accuracy, speed) {
   table.push([chalk.green(accuracy), chalk.green(speed)]);
   return table.toString();
 };
 
-module.exports = {format};
+module.exports = { format };
